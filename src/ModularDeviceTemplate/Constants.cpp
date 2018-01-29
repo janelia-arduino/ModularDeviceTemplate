94a8df1933540425384976af11956f4fb56ab694
Peter Polidoro <peterpolidoro@gmail.com>
2018-01-29 14:05:35 -0500

    Updated spacing.

--- src/ModularDeviceTemplate/Constants.cpp
+++ src/ModularDeviceTemplate/Constants.cpp
@@ -17,12 +17,12 @@ CONSTANT_STRING(device_name,"modular_device_template");
 CONSTANT_STRING(firmware_name,"ModularDeviceTemplate");
 // Use semantic versioning http://semver.org/
 const modular_server::FirmwareInfo firmware_info =
-  {
-    .name_ptr=&firmware_name,
-    .version_major=1,
-    .version_minor=0,
-    .version_patch=0,
-  };
+{
+  .name_ptr=&firmware_name,
+  .version_major=1,
+  .version_minor=0,
+  .version_patch=0,
+};
 
 // Interrupts
 
