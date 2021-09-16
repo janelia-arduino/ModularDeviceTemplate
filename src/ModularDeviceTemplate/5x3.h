b6bc85db0b86fbafc560e233bcaeb3b884266adb
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 14:13:34 -0400

    Update email address

--- src/ModularDeviceTemplate/5x3.h
+++ src/ModularDeviceTemplate/5x3.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef MODULAR_DEVICE_TEMPLATE_5X3_CONSTANTS_H
 #define MODULAR_DEVICE_TEMPLATE_5X3_CONSTANTS_H
